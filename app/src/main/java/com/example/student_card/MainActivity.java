7428bfc6e6e9599a3b9ed439d2bc26e9ba28ccb5
miyigun2017 <miyigun2017@gmail.com>
2022-08-06 22:15:40 +0300

    third commit

--- app/src/main/java/com/example/student_card/MainActivity.java
+++ app/src/main/java/com/example/student_card/MainActivity.java
@@ -31,10 +31,10 @@ public class MainActivity extends AppCompatActivity {
     private void fillList() {
         studentList=new ArrayList<>();
 
-        StudentCardModel card1=new StudentCardModel(R.drawable.student1,"12345678910","Ayşe","Beautiful","xy","xx","123","class 9/A","Karataş","01234567891");
-        StudentCardModel card2=new StudentCardModel(R.drawable.student2,"12345678910","Sevgi","Beautiful","xy","xx","123","class 9/A","Karataş","01234567891");
-        StudentCardModel card3=new StudentCardModel(R.drawable.student3,"12345678910","Animal1","Beautiful","xy","xx","123","class 9/A","Karataş","01234567891");
-        StudentCardModel card4=new StudentCardModel(R.drawable.student2,"12345678910","Animal2","Beautiful","xy","xx","123","class 9/A","Karataş","01234567891");
+        StudentCardModel card1=new StudentCardModel(R.drawable.student1,"12345678910","Ayşe","Beautiful","xy","xx","001","class 9/A","Karataş","01234567891");
+        StudentCardModel card2=new StudentCardModel(R.drawable.student2,"12345678910","Sevgi","Beautiful","xy","xx","002","class 9/B","Akkent","12345678910");
+        StudentCardModel card3=new StudentCardModel(R.drawable.student3,"12345678910","Animal","Squirrel","xy","xx","003","class 9/C","Yeditepe","23456789101");
+        StudentCardModel card4=new StudentCardModel(R.drawable.student4,"12345678910","Animal","Sheep","xy","xx","004","class 9/D","Bülbülzade","34567891011");
 
         studentList.add(card1);
         studentList.add(card2);
